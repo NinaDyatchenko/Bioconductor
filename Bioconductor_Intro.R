4c1d849540cebaa363bbf7b9bb9a4ebc15dc843f
NinaDyatchenko <nxd136@miami.edu>
2019-06-02 09:57:23 -0400

    commit

---
+++ Bioconductor_Intro.R
@@ -0,0 +1,10 @@
+source("https://bioconductor.org/biocLite.R")
+biocLite("BSgenome")
+library("BSgenome")
+packageVersion("BSgenome")
+isS4(packageVersion)
+
+showClass("BSgenome")
+
+class(a_genome)
+browseVignettes(package="Biobase")
